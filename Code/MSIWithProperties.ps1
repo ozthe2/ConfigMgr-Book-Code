2f2c3020daaca9872aeff8954e6fbce782764038
OzThe2 <owenthe2@gmail.com>
2019-03-26 16:41:16 +0000

    add code

---
+++ Code/MSIWithProperties.ps1
@@ -0,0 +1,1 @@
+﻿Start-Process -FilePath "$ENV:SystemRoot\System32\msiexec.exe" -ArgumentList "/i $WorkingDir\jre1.8.0_191.msi /qn JAVAUPDATE=0 AUTOUPDATECHECK=0 IEXPLORER=1 REBOOT=Suppress" -NoNewWindow -Wait
---
+++ Code/SilentEXE.ps1
@@ -0,0 +1,1 @@
+﻿start-process -FilePath ".\7z1900-x64.exe" -ArgumentList "/S" -NoNewWindow -Wait
