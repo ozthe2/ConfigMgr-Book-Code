537cf812295c15c6814bedeeeedb5f794f0eb74b
OzThe2 <owenthe2@gmail.com>
2019-04-22 19:39:29 +0100

    Added new ccm resize code

---
+++ Code/Part9/SetCCMCache/DetectionRule.ps1
@@ -0,0 +1,7 @@
+﻿$CacheSize = #Enter the value that you set the cache size to here eg 10240
+
+$CCM = New-Object -ComObject UIResource.UIResourceMGR
+
+if (($CCM.GetCacheInfo()).totalsize -eq $CacheSize) {
+    Write-Host "Installed!"
+}
