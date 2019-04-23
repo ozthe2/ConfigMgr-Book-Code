dfba5f4029109a490ab9872cec96d36ab120437a
OzThe2 <ozthe2@users.noreply.github.com>
2019-04-23 16:43:25 +0100

    Update Set-CCMCacheSIze.ps1

--- Code/Part9/SetCCMCache/Set-CCMCacheSIze.ps1
+++ Code/Part9/SetCCMCache/Set-CCMCacheSIze.ps1
@@ -1,10 +1,6 @@
-﻿function Set-CCMCacheSize {
+Function Set-CCMCacheSize {
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory=$false)]
-        [String]
-        $ComputerName = 'localhost',
-        
         [Parameter(Mandatory=$true,
         HelpMessage = "Enter the number to resize the cache to in megabytes. ie it must be divisible by 1024. Eg. to set to 6GB enter 6144")]
         [ValidateScript({$_ % 1024 -eq 0})]
